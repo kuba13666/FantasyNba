58c10ba3dacf917839cb8a6c91c4c7e7e6fde403
Jakub Lorenc <kuba.lorenc2@gmail.com>
2017-12-13 20:47:16 +0100

    PassingCustomerToTheNewTeam

--- FantasyNBA/FantasyNBA/Controllers/TeamsController.cs
+++ FantasyNBA/FantasyNBA/Controllers/TeamsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FantasyNBA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,18 @@ namespace FantasyNBA.Controllers
 {
     public class TeamsController : Controller
     {
-        // GET: Teams
-        public ActionResult New()
+
+        private ApplicationDbContext _context;
+
+        public TeamsController()
         {
-            return View();
+            _context = new ApplicationDbContext();
+        }
+        // GET: Teams
+        public ActionResult New(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            return View(customer);
 
         }
     }
